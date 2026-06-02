3f8b908ee4061326e8a3b5952fb772dfbc4dc71d
atuk6092 <atuk6092@gmail.com>
2026-06-02 23:32:49 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-// Bu dosya artık kullanılmıyor.
-// Tüm push bildirim işlemleri OneSignalSDKWorker.js tarafından yönetiliyor.
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
-  self.registration.unregister();
-});
